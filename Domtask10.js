24f91270551f679136a5e6301cf3906006eb4471
karthik-140 <shanigaramkarthik@gmail.com>
2022-12-29 15:56:56 +0530

    localstoragetask12correctsolution

--- Domtask10.js
+++ Domtask10.js
@@ -3,19 +3,21 @@ form.addEventListener('submit', onsubmit);
 function onsubmit(e){
     e.preventDefault();
 
-    const ans={
-        name:e.target.name.value,
-        Email:e.target.email.value
-    };  
+    const name=e.target.name.value;
+    const Email=e.target.email.value;
+    const ans=`{
+        ${name},
+        ${Email}
+    };`
     var json=JSON.stringify(ans);
-    localStorage.setItem(ans.Email,json);
+    localStorage.setItem(Email,json);
     
     
 
     var showuser=document.querySelector('.container');
     var li=document.createElement('li');
     li.className='list-group-item';
-    li.appendChild(document.createTextNode(json));
+    li.appendChild(document.createTextNode(name+" "+Email));
 
     showuser.appendChild(li);
           

d4237a5a661bb40de97c8863b1a90d055054e696
karthik-140 <shanigaramkarthik@gmail.com>
2023-05-17 10:32:25 +0530

    DSA-&-sorting-algo

--- DSA.js
+++ DSA.js
@@ -178,3 +178,164 @@
 
 // Component(Products);
 
+//                                            Second Value
+
+// const sencondValue = (nums) =>{
+//     const sortedNums = [...new Set(nums)].sort((a,b)=>{
+//         return a-b;
+//     });
+//     console.log(`sorted nums --> ${sortedNums}`);
+
+//     if(sortedNums.length <= 1){
+//         return sortedNums[0];
+//     }else if(sortedNums.length === 2){
+//         return `${sortedNums[0]} ${sortedNums[1]}`
+//     }
+//     if(sortedNums.length === 3){
+//         return sortedNums[1];
+//     } 
+//     // let ans=[];
+//     // ans.push(sortedNums[1]);
+//     // ans.push(sortedNums[sortedNums.length-2]);
+//     return `${sortedNums[1]} ${sortedNums[sortedNums.length-2]}`;
+// }
+
+// console.log(sencondValue([2,3,5,4,6]));
+// console.log(sencondValue([2]));
+// console.log(sencondValue([5,8]));
+// console.log(sencondValue([5,4,6]));
+// console.log(sencondValue([3,3,-9,4,5]));
+// console.log(sencondValue([3,98,6,8,6,-5]));
+
+//  Bubble sort
+// const nums = [2,3,7,5,1,4,9,6,8,10];
+
+// for(let i=0;i<nums.length;i++){
+//     for(let j=0;j<nums.length-1;j++){
+//         if(nums[j] > nums[j+1]){
+//             let temp=nums[j];
+//             nums[j]=nums[j+1];
+//             nums[j+1]=temp;
+//         }
+//     }
+// }
+// console.log(nums);
+
+
+// // Insertion sort
+// const nums = [2,3,7,5,1,4,9,6,8,10];
+
+// for(let i=1;i<nums.length;i++){
+//     let temp=nums[i];
+//     let j=i-1;
+//     while(j>=0 && temp<nums[j]){
+//         nums[j+1]=nums[j];
+//         j=j-1;
+//     }
+//     nums[j+1]=temp;
+// }
+// console.log(nums);
+
+// Selction Sort
+// const nums = [2,3,7,5,1,4,9,6,8,10];
+
+// for(let i=0;i<nums.length-1;i++){
+//     let index=i;
+//     for(let j=i+1;i<nums.length;j++){
+//        if(nums[j]< nums[index]){
+//         index=j;
+//        }
+//     }
+//     // let temp=nums[i];
+//     // nums[i]=nums[index];
+//     // nums[index]=temp;
+//     [nums[i], nums[index]] = [nums[index], nums[i]];    
+// }
+
+// console.log(nums);
+
+//Merge sort
+// const nums = [2,3,7,5,1,4,9,6,8,10];
+
+// const MergeSort = (arr) =>{
+//     let n=arr.length;
+//     if(n <=1 ){
+//         return arr;
+//     }
+//     let mid=Math.floor(n/2);
+//     let left = MergeSort(arr.slice(0,mid));
+//     let right = MergeSort(arr.slice(mid));
+//     return Merge(left, right);
+// }
+
+// const Merge = (left, right) => {
+//     let result = [];
+//     let i=0;
+//     let j=0;
+//     if(left[i] < right[j]){
+//         result.push(left[i]);
+//         i++;
+//     }else{
+//         result.push(right[j]);
+//         j++;
+//     }
+//     return result.concat(left.slice(i).concat(right.slice(j)));
+// }
+// console.log(MergeSort(nums));
+
+// const arr = [2,3,7,5,1,4,9,6,8,10];
+
+// const MergeSort = (arr) =>{
+//     let n=arr.length;
+//     if(n<=1){
+//         return arr;
+//     }
+//     let mid= Math.floor(n/2);
+//     let left=MergeSort(arr.slice(0,mid));
+//     let right=MergeSort(arr.slice(mid));
+//     return Merge(left, right);
+// }
+
+// const Merge = (left, right) =>{
+//     let result = [];
+//     let i=0;
+//     let j=0;
+//     if(left[i] < right[j]){
+//         result.push(left[i]);
+//         i++;
+//     }else{
+//         result.push(right[j]);
+//         j++;
+//     }
+//     return result.concat(left.slice(i)).concat(right.slice(j));
+// }
+
+// console.log(MergeSort(arr));
+
+// const arr = [2,3,7,5,1,4,9,6,8,10];
+
+// const QuickSort = (arr) =>{
+//     let n=arr.length;
+//     if(n<=1){
+//         return arr;
+//     }
+//     let pivotIndex = Math.floor(n/2);
+//     let pivotValue = arr[pivotIndex];
+//     let left = [];
+//     let right = [];
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i] === pivotValue){
+//            continue; 
+//         }
+//         if(arr[i] < pivotValue){
+//             left.push(arr[i]);
+//         }else{
+//             right.push(arr[i]);
+//         }
+//     }
+//     return [...QuickSort(left), pivotValue, ...QuickSort(right)];
+// }
+
+// console.log(QuickSort(arr));
+
+
